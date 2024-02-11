3cc1e22f306222b7e178b9328c94c6baff0d3a31
daxi0823 <daxi0823@gmail.com>
2024-02-11 16:55:27 +0900

    型の導入

--- react-lesson/src/main.tsx
+++ react-lesson/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// as Element:型アサーション（推論） createRootにNullとなりえる値を渡せないため。
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
