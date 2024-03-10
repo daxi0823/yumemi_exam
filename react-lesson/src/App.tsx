76689ea680705c5f91ea46e9c163095e4bae09cd
daxi <daxi0823@gmail.com>
2024-03-10 17:18:10 +0900

    不要なcss削除

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -8,15 +8,17 @@ import { Routing } from "./routes/Router";
 function App() {
   const [count, setCount] = useState<number>(0);
 
-  return (
-    <div>
-      <div>
-        <h2>都道府県</h2>
-        <div>{/* チェックボックス */}</div>
-      </div>
-      <Routing />
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<h2 className="underline">都道府県</h2>
+				<div className="p-4 bg-gray-200">
+					{/* チェックボックス */}
+				</div>
+			</div>
+			<Routing />
+		</div>
+	);
 }
 
 export default App;
