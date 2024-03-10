a64a162693054cba5f6908275ce4ea8ee2cf0e4f
daxi <daxi0823@gmail.com>
2024-03-10 15:49:45 +0900

    名前の修正

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Router } from "react-router-dom";
-import { Routing } from "./routes/Routes"
+import { Routing } from "./routes/Router"
 
 function App() {
   const [count, setCount] = useState<number>(0);
