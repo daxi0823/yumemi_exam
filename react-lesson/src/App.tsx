89c99611b251a8ddda71d512b70c0b6013ff841b
daxi <daxi0823@gmail.com>
2024-03-10 17:02:22 +0900

    sudo yarn add prettier --dev --exact & yarn prettier . --write

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Router } from "react-router-dom";
-import { Routing } from "./routes/Router"
+import { Routing } from "./routes/Router";
 
 function App() {
   const [count, setCount] = useState<number>(0);
@@ -14,7 +14,7 @@ function App() {
         <h2>都道府県</h2>
         <div>{/* チェックボックス */}</div>
       </div>
-			<Routing />
+      <Routing />
     </div>
   );
 }
